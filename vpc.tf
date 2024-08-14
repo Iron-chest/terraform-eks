f795a46baaea34e2afd4e327930ee4e3c8e1ab46
AKINTAYO TOLANI <devopsforjesus@gmail.com>
2024-08-14 03:11:39 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,19 +6,19 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "abhi-eks-${random_string.suffix.result}"
+  cluster_name = "demo-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
-  length  = 8
+  length  = 5
   special = false
 }
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.7.0"
+  version = "5.12.0"
 
-  name                 = "abhi-eks-vpc"
+  name                 = "demo-eks-vpc"
   cidr                 = var.vpc_cidr
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
