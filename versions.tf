dedfd3afdd61c3126b7299f212d82156f3d42e27
AKINTAYO TOLANI <devopsforjesus@gmail.com>
2024-08-14 03:10:33 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -6,10 +6,6 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.1.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">=2.7.1"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 3.68.0"
@@ -21,10 +17,6 @@ terraform {
     null = {
       source  = "hashicorp/null"
       version = "~> 3.1.0"
-    }
-    cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = "~> 2.2.0"
     }
   }
 }
